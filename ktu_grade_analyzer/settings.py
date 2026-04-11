8aa4118be040ef83d07c98ae6c123c4475cadb39
Anna-Jaison <anaajai0704@gmail.com>
2026-04-11 10:23:41 +0530

    fix: configure DATABASES with dj-database-url

--- ktu_grade_analyzer/settings.py
+++ ktu_grade_analyzer/settings.py
@@ -82,7 +82,10 @@ WSGI_APPLICATION = "ktu_grade_analyzer.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 # Password validation
